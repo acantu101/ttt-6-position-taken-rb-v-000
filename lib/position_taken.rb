4031030d4cceb2af46ee3eb14b35df15626553cf
alejandra cantu <alejandra.cantu13@gmail.com>
2018-11-07 04:19:22 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 def position_taken?(board,index)
   if index = " " || index = ""
     false
-  elsif board[index] == "X" || "O"
+  elsif board[index] == "X" || board[index] "O"
   true
   end
 end    
